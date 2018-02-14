9273bbc2216f90aada951395f0f5b5246801a743
Caree Claunch <caree@caree.codes>
2018-02-13 20:52:53 -0600

    Added updateable image element. Still does not take user input, using a test image right now

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 console.log('working');
+
+var userImage = "https://cdn.shopify.com/s/files/1/0808/5563/products/prod-pizza-cheese-slice-lg.jpg?v=1503592252";
+var testButton = document.querySelector("#test_button");
+var shrineSubject = document.querySelector("#shrine_subject")
+
+
+testButton.addEventListener("click", function(){
+    shrineSubject.setAttribute("src", userImage);
+})
+
