a5dc2f84bfa545cb25680d37bf22752ea06db54a
Caree Claunch <caree@caree.codes>
2018-02-14 21:52:09 -0600

    Very terrible but functional image resize ability

--- js/index.js
+++ js/index.js
@@ -1,17 +1,34 @@
+// Get User Image and Display
+// ============================================================================
 var uploadButton = document.querySelector("#image_upload_button");
 var uploadedImage = document.querySelector("#uploaded_image");
 
 function getUserImage() {
-      var file    = document.querySelector('input[type=file]').files[0];
-      var reader  = new FileReader();
-    
-      reader.addEventListener("load", function () {
-        uploadedImage.src = reader.result;
-      }, false);
-    
-      return reader.readAsDataURL(file);
-    }
+  var file    = document.querySelector('input[type=file]').files[0];
+  var reader  = new FileReader();
 
+  reader.addEventListener("load", function () {
+    uploadedImage.src = reader.result;
+  }, false);
+
+  return reader.readAsDataURL(file);
+}
+
+// Set the image in the "preview"
 uploadButton.addEventListener("change", function(){
     uploadedImage.setAttribute("alt", getUserImage());
+})
+
+// Resize Image
+// ============================================================================
+var imageWidth = uploadedImage.clientWidth;
+
+document.querySelector("#increase_size").addEventListener("click", function(){
+    imageWidth += 5;
+    uploadedImage.setAttribute("style", "width:" + imageWidth + "px; height: auto;");
+})
+
+document.querySelector("#decrease_size").addEventListener("click", function(){
+    imageWidth -= 5;
+    uploadedImage.setAttribute("style", "width:" + imageWidth + "px; height: auto;");
 })
